7731e23eaa306a9f51a10bf7a0af5fc3a8384d2b
vororo <adamolsson@outlook.com>
2023-11-24 15:38:46 +0900

    feat: Basic sending and receiving of strings and displaying them in a list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // MODULES IMPORT
 import { Metadata } from 'next';
 import HomeRedirect from './home-redirect';
+import ChatRoomSub from './chat-room/chat-room-component';
 
 // PAGE NAME
 export const metadata: Metadata = {
@@ -12,6 +13,7 @@ export default function Home(): JSX.Element {
     return (
         <div className="home">
             <HomeRedirect />
+            <ChatRoomSub/>
         </div>
     )
 }
