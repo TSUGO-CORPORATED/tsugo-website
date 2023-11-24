9888783cbacfac2c083e0297619d56ebc02465d9
vororo <adamolsson@outlook.com>
2023-11-24 17:48:55 +0900

    edit: removed unused email context variable from layout-sub

--- src/app/layout-sub.tsx
+++ src/app/layout-sub.tsx
@@ -15,12 +15,11 @@ interface Props {
 // PAGE COMPONENT
 export default function LayoutSub(props: Props): JSX.Element {
     const [userId, setUserId] = useState<number>(0);
-    const [userEmail, setUserEmail] = useState<string>('testEmail');
     const [userFirstName, setUserFirstName] = useState<string>('testFirstName');
     const [userLastName, setUserLastName] = useState<string>('testLastName');
 
     return (
-        <ContextVariables.Provider value={{userId, userEmail, userFirstName, userLastName, setUserId, setUserEmail, setUserFirstName, setUserLastName}}>
+        <ContextVariables.Provider value={{userId, userFirstName, userLastName, setUserId, setUserFirstName, setUserLastName}}>
             <body className={`${inter.className}`}>
                 <div className='navbar-layout'><Navbar /></div>
                 <div className='childrenprops-layout'>{props.childrenProp}</div>
