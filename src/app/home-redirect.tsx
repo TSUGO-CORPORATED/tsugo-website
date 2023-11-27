4f6c373cd9e9eaaf5c7c09d382fee2f91f052e56
vororo <adamolsson@outlook.com>
2023-11-27 14:50:14 +0900

    feat: client side now can parse and display messages sent from a socket on the server

--- src/app/home-redirect.tsx
+++ src/app/home-redirect.tsx
@@ -3,6 +3,7 @@
 // MODULES IMPORT
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import ChatRoomSub from "./chat-room/chat-room-component"
 
 // PAGE COMPONENT
 export default function HomeRedirect(): JSX.Element {
@@ -10,6 +11,7 @@ export default function HomeRedirect(): JSX.Element {
     return (
         <>
             <Link href="/log-in" className='sign-up__card__log-in-link'><p>Go to Log In</p></Link>
+            <ChatRoomSub/>
         </>
     )
 }
