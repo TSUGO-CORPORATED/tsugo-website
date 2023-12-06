dd74a22718ccf3499f091965b9c77c376034a779
Anthonio Hagi Tobing <133083562+anthoniohagitobing@users.noreply.github.com>
2023-12-06 12:18:02 +0900

    feat: add colors

--- src/app/global/appointment-block.tsx
+++ src/app/global/appointment-block.tsx
@@ -31,63 +31,63 @@ export default function AppointmentBlock({appointment}: {appointment: Appointmen
                 const tempDateTime = appointment.appointmentDateTime;
                 // console.log(tempDateTime)
                 const dateObject = new Date(tempDateTime)
-                const convertedDateTime = tempDateTime ?format(dateObject, "EEE, dd MMM yy HH:mm") : null;
+                const convertedDateTime = tempDateTime ?format(dateObject, "EEE, dd MMM yy") : null;
 
                 return (
                     <div key={index} className='appointment-block'>
                         <Paper elevation={2}>
-                            {/* <Box sx={{ flexGrow: 1 }}> */}
-                                <Grid container spacing={1}>
-                                    <Grid xs={6}>
-                                        <div className='appointment-block__info__row__piece'>
-                                            <label className='appointment-block__info__row__piece__label'>ID:</label>
-                                            <p className='appointment-block__info__row__piece__data'>{appointment.id}</p>
-                                        </div>
-                                    </Grid>
-                                    <Grid xs={6}>
-                                        <div className='appointment-block__info__row__piece'>
-                                            <label className='appointment-block__info__row__piece__label'>Status:</label>
-                                            <p className='appointment-block__info__row__piece__data'>{appointment.status}</p>
-                                        </div>
-                                    </Grid>
-                                    <Grid xs={6}>
-                                        <div className='appointment-block__info__row__piece'>
-                                            <label className='appointment-block__info__row__piece__label'>Date:</label>
-                                            <p className='appointment-block__info__row__piece__data'>{convertedDateTime}</p>
-                                        </div>
-                                    </Grid>
-                                    <Grid xs={6}>
-                                        <div className='appointment-block__info__row__piece'>
-                                            <label className='appointment-block__info__row__piece__label'>Type:</label>
-                                            <p className='appointment-block__info__row__piece__data'>{appointment.appointmentType}</p>
-                                        </div>
-                                    </Grid>
-                                    <Grid xs={12}>
-                                        <div className='appointment-block__info__row__piece'>
-                                            <label className='appointment-block__info__row__piece__label'>Title:</label>
-                                            <p className='appointment-block__info__row__piece__data'>{appointment.appointmentTitle}</p>
-                                        </div>
-                                    </Grid>
-                                    <Grid xs={12}>
-                                        <div className='appointment-block__info__row__piece'>
-                                            <label className='appointment-block__info__row__piece__label'>Location:</label>
-                                            <p className='appointment-block__info__row__piece__data'>{appointment.locationName}</p>
-                                        </div>
-                                    </Grid>
-                                    <Grid xs={12}>
+                            <Grid container spacing={1}>
+                                <Grid xs={6}>
+                                    <div className='appointment-block__info__row__piece'>
+                                        <label className='appointment-block__info__row__piece__label'>ID:</label>
+                                        <p className='appointment-block__info__row__piece__data'>{appointment.id}</p>
+                                    </div>
+                                </Grid>
+                                <Grid xs={6}>
+                                    <div className='appointment-block__info__row__piece'>
+                                        <label className='appointment-block__info__row__piece__label'>Status:</label>
+                                        <p className='appointment-block__info__row__piece__data'>{appointment.status}</p>
+                                    </div>
+                                </Grid>
+                                <Grid xs={6}>
+                                    <div className='appointment-block__info__row__piece'>
+                                        <label className='appointment-block__info__row__piece__label'>Date:</label>
+                                        <p className='appointment-block__info__row__piece__data'>{convertedDateTime}</p>
+                                    </div>
+                                </Grid>
+                                <Grid xs={6}>
+                                    <div className='appointment-block__info__row__piece'>
+                                        <label className='appointment-block__info__row__piece__label'>Type:</label>
+                                        <p className='appointment-block__info__row__piece__data'>{appointment.appointmentType}</p>
+                                    </div>
+                                </Grid>
+                                <Grid xs={12}>
+                                    <div className='appointment-block__info__row__piece'>
+                                        <label className='appointment-block__info__row__piece__label'>Title:</label>
+                                        <p className='appointment-block__info__row__piece__data'>{appointment.appointmentTitle}</p>
+                                    </div>
+                                </Grid>
+                                <Grid xs={12}>
+                                    <div className='appointment-block__info__row__piece'>
+                                        <label className='appointment-block__info__row__piece__label'>Location:</label>
+                                        <p className='appointment-block__info__row__piece__data'>{appointment.locationName ? appointment.locationName : '-' }</p>
+                                    </div>
+                                </Grid>
+                                <Grid xs={12}>
+                                    <div className="appointment-block__detail">
                                         <Link href={{
                                             pathname: '/appointment-detail',
                                             query: {appointmentId: appointment.id}
-                                        }} className="appointment-block__detail-link">
-                                            <Button variant='contained' sx={buttonBlack} size='small' className="appointment-block__detail-link__button">
-                                                <div className="appointment-block__detail-link__button__text">
+                                        }} className="appointment-block__detail__link">
+                                            <Button variant='contained' sx={buttonBlack} size='small' className="appointment-block__detail__link__button">
+                                                <div className="appointment-block__detail__link__button__text">
                                                     <p>See details</p>
                                                 </div>
                                             </Button>
-                                        </Link>
-                                    </Grid>
+                                        </Link> 
+                                    </div>
                                 </Grid>
-                            {/* </Box> */}
+                            </Grid>
                         </Paper>
                     </div>
                 )
